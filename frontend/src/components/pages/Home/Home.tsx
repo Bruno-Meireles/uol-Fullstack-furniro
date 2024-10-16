07cb4b3f57328638de6b84fdbf6cda430efde6ec
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-10-16 16:30:35 -0300

    Adding card products

--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner/Banner";
 import Browser from "./Browse/Browse";
+import Product from "./Product/Product";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Banner />
       <Browser />
+      <Product />
     </div>
   );
 };
---
+++ frontend/src/components/pages/Home/Product/Product.tsx
@@ -0,0 +1,73 @@
+import "./Product.css";
+
+const products = [
+  {
+    id: 1,
+    name: "Sytherine",
+    description: "Stylish cafe chair",
+    price: "R$ 2.500.000",
+    originalPrice: "R$ 3.500.000",
+    discount: "-30%",
+    image: "/assets/images/Syltherine.png",
+  },
+  {
+    id: 2,
+    name: "Leviosa",
+    description: "Stylish cafe chair",
+    price: "R$ 2.500.000",
+    image: "/assets/images/leviosa.png",
+  },
+  {
+    id: 3,
+    name: "Respira",
+    description: "Luxury big sofa",
+    price: "R$ 7.000.000",
+    originalPrice: "R$ 14.000.000",
+    discount: "-50%",
+    image: "/assets/images/respira.png",
+  },
+  {
+    id: 4,
+    name: "Potty",
+    description: "Minimalist flower pot",
+    price: "R$ 5.000.000",
+    originalPrice: "R$ 14.000.000",
+    // discount: "-50%",
+    image: "/assets/images/potty.png",
+    isNew: true,
+  },
+];
+
+const Product: React.FC = () => {
+  return (
+    <section>
+      <div className="product-content">
+        <h1>Our Products</h1>
+        <div className="product-flex">
+          {products.map((product) => (
+            <div className="product-card" key={product.id}>
+              <div className="product-image">
+                <img src={product.image} alt={product.name} />
+                {product.discount && (
+                  <span className="discount">{product.discount}</span>
+                )}
+                {product.isNew && <span className="new">New</span>}
+              </div>
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <p className="price">{product.price}</p>
+              {product.originalPrice && (
+                <p className="original-price">
+                  <s>{product.originalPrice}</s>
+                </p>
+              )}
+              <button className="button-details">See Details</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
