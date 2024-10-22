998f78b160ec216ab69f1c9f45c34f1d27b1ae25
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-10-22 11:32:25 -0300

    finishing static part of the feat/barSinglePage in nav-bar

--- frontend/src/pages/SingleProductPage/SingleProductPage.tsx
+++ frontend/src/pages/SingleProductPage/SingleProductPage.tsx
@@ -1,13 +1,27 @@
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
+import { Link } from "react-router-dom";
 import ProductsList from "../Home/ProductList/ProductList";
+import "./SingleProductPage.css";
 
 const SingleProductPage: React.FC = () => {
   return (
     <div>
-      <Header />
-          <ProductsList title="Related Products" limit={4} isSinglePage={true} />
-          <Footer />
+      <section className="singleBar">
+        <div className="content">
+          <nav className="singleBarItems">
+            <Link className="singleBarLinks" to={"/"}>
+              Home
+            </Link>
+            <img src="/assets/icons/arrow.svg" alt="" />
+            <Link className="singleBarLinks" to={"/shop"}>
+              Shop
+            </Link>
+            <img src="/assets/icons/arrow.svg" alt="" />
+            <img src="assets/icons/line.svg" alt="" />
+            <p >Asgaard sofa</p>
+          </nav>
+        </div>
+      </section>
+      <ProductsList title="Related Products" limit={4} isSinglePage={true} />
     </div>
   );
 };
