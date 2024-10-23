64ccf1e1419ded704fb35b1604375c8775e39e14
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-10-23 02:35:27 -0300

    Create Component ProductDetailItem

---
+++ frontend/src/pages/SingleProductPage/ProductDetailItem/ProductDetailItem.tsx
@@ -0,0 +1,140 @@
+import React from "react";
+import "./ProuctDetailItem.css"
+
+interface ProductDetailItemProps {
+  product: Product;
+  quantity: number;
+  handleQuantityChange: (action: string) => void;
+}
+
+const ProductDetailItem: React.FC<ProductDetailItemProps> = ({
+  product,
+  quantity,
+  handleQuantityChange,
+}) => {
+  return (
+    <section className="see-detail-product">
+      <div className="content">
+        <div className="container-detail">
+          <div className="other-images">
+            <img
+              className="other-images-item"
+              src={product.image_link} 
+              alt={product.name}
+            />
+          </div>
+
+          <div className="image-big">
+            <img
+              className="image-big-detail"
+              src={product.image_link}
+              alt={product.name}
+            />
+          </div>
+
+          <div className="detalhes--grandes">
+            <h3 className="detail-title">{product.name}</h3>
+            <span className="price-customer">
+              {new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(parseFloat(product.price))}
+            </span>
+            <div className="detail-price-customer">
+              <img
+                className="detail-stars"
+                src="/assets/icons/stars.svg"
+                alt="stars"
+              />
+              <img
+                className="detail-bar"
+                src="/assets/icons/line.svg"
+                alt=" bar"
+              />
+              <p className="detail-customer">5 Customer Review</p>
+            </div>
+            <p className="detail-paragraph">{product.description}</p>
+            <div className="product-options">
+              <div className="sizes">
+                <span className="size-title">Size</span>
+                <div className="size-item">
+                  <button className="size-items">L</button>
+                  <button className="size-items">XL</button>
+                  <button className="size-items">XS</button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <span className="circle-title">Color</span>
+              <div className="circle-container">
+                <div className="circle purple-circle"></div>
+                <div className="circle black-circle"></div>
+                <div className="circle yellow-circle"></div>
+              </div>
+            </div>
+            <div className="product-actions">
+              <div className="quantity">
+                <button
+                  className="quantity-button"
+                  onClick={() => handleQuantityChange("decrease")}
+                >
+                  -
+                </button>
+                <span>{quantity}</span>
+                <button
+                  className="quantity-button"
+                  onClick={() => handleQuantityChange("increase")}
+                >
+                  +
+                </button>
+              </div>
+              <button className="add-to-cart">Add To Cart</button>
+              <button className="compare">+ Compare</button>
+            </div>
+            <div className="detail-separador"></div>
+            <div className="product-meta">
+              <div className="product-meta-detail">
+                <span className="label">SKU</span>
+                <span className="value">: SS001</span>
+              </div>
+              <div className="product-meta-detail">
+                <span className="label">Category</span>
+                <span className="value">: Sofas</span>
+              </div>
+              <div className="product-meta-detail">
+                <span className="label">Tags</span>
+                <span className="value">: Sofa, Chair, Home, Shop</span>
+              </div>
+              <div className="share">
+                <span className="label">Share</span>
+                <span className="value-share">:</span>
+                <a href="#">
+                  <img src="/assets/icons/face.svg" alt="Icon Facebook" />
+                </a>
+                <a href="#">
+                  <img src="/assets/icons/linkedin.svg" alt="Icon Linkedin" />
+                </a>
+                <a href="#">
+                  <img src="/assets/icons/twitter.svg" alt="Icon Twitter" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  discount_price?: string;
+  discount_percent?: string;
+  image_link: string;
+  is_new?: boolean;
+}
+
+export default ProductDetailItem;
--- frontend/src/pages/SingleProductPage/SingleProductPage.tsx
+++ frontend/src/pages/SingleProductPage/SingleProductPage.tsx
@@ -1,8 +1,9 @@
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import ProductsList from "../Home/ProductList/ProductList";
-import React, { useEffect, useState } from "react";
-import "./SingleProductPage.css";
 import axios from "axios";
+import "./SingleProductPage.css";
+import ProductDetailItem from "./ProductDetailItem/ProductDetailItem";
 
 const SingleProductPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -46,130 +47,14 @@ const SingleProductPage: React.FC = () => {
             </Link>
             <img src="/assets/icons/arrow.svg" alt="arrow" />
             <img src="/assets/icons/line.svg" alt="line" />
-            <p>{product.name}</p> {/* Produto dinâmico */}
+            <p>{product.name}</p>
           </nav>
 
-          <section className="see-detail-product">
-            <div className="content">
-              <div className="container-detail">
-                <div className="other-images">
-                  {/* Imagens adicionais - você pode substituir essas URLs se sua API retornar outras imagens */}
-                  <img
-                    className="other-images-item"
-                    src={product.image_link} // Imagem principal
-                    alt={product.name}
-                  />
-                </div>
-
-                <div className="image-big">
-                  <img
-                    className="image-big-detail"
-                    src={product.image_link}
-                    alt={product.name}
-                  />
-                </div>
-
-                <div className="detalhes--grandes">
-                  <h3 className="detail-title">{product.name}</h3>{" "}
-                  {/* Nome do produto */}
-                  <span className="price-customer">
-                    {new Intl.NumberFormat("pt-BR", {
-                      style: "currency",
-                      currency: "BRL",
-                    }).format(parseFloat(product.price))}
-                  </span>{" "}
-                  {/* Preço formatado */}
-                  <div className="detail-price-customer">
-                    <img
-                      className="detail-stars"
-                      src="/assets/icons/stars.svg"
-                      alt="stars"
-                    />
-                    <img
-                      className="detail-bar"
-                      src="assets/icons/line.svg"
-                      alt=" bar"
-                    />
-                    <p className="detail-customer">5 Customer Review</p>
-                  </div>
-                  <p className="detail-paragraph">
-                    {product.description} {/* Descrição do produto */}
-                  </p>
-                  <div className="product-options">
-                    <div className="sizes">
-                      <span className="size-title">Size</span>
-                      <div className="size-item">
-                        <button className="size-items">L</button>
-                        <button className="size-items">XL</button>
-                        <button className="size-items">XS</button>
-                      </div>
-                    </div>
-                  </div>
-                  <div>
-                    <span className="circle-title">Color</span>
-                    <div className="circle-container">
-                      <div className="circle purple-circle"></div>
-                      <div className="circle black-circle"></div>
-                      <div className="circle yellow-circle"></div>
-                    </div>
-                  </div>
-                  <div className="product-actions">
-                    <div className="quantity">
-                      <button
-                        className="quantity-button"
-                        onClick={() => handleQuantityChange("decrease")}
-                      >
-                        -
-                      </button>
-                      <span>{quantity}</span>
-                      <button
-                        className="quantity-button"
-                        onClick={() => handleQuantityChange("increase")}
-                      >
-                        +
-                      </button>
-                    </div>
-                    <button className="add-to-cart">Add To Cart</button>
-                    <button className="compare">+ Compare</button>
-                  </div>
-                  <div className="detail-separador"></div>
-                  <div className="product-meta">
-                    <div className="product-meta-detail">
-                      <span className="label">SKU</span>
-                      <span className="value">: SS001</span>
-                    </div>
-                    <div className="product-meta-detail">
-                      <span className="label">Category</span>
-                      <span className="value">: Sofas</span>
-                    </div>
-                    <div className="product-meta-detail">
-                      <span className="label">Tags</span>
-                      <span className="value">: Sofa, Chair, Home, Shop</span>
-                    </div>
-                    <div className="share">
-                      <span className="label">Share</span>
-                      <span className="value-share">:</span>
-                      <a href="#">
-                        <img src="/assets/icons/face.svg" alt="Icon Facebook" />
-                      </a>
-                      <a href="#">
-                        <img
-                          src="/assets/icons/linkedin.svg"
-                          alt="Icon Linkedin"
-                        />
-                      </a>
-                      <a href="#">
-                        <img
-                          src="/assets/icons/twitter.svg"
-                          alt="Icon Twitter"
-                        />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
+          <ProductDetailItem
+            product={product}
+            quantity={quantity}
+            handleQuantityChange={handleQuantityChange}
+          />
         </div>
       </section>
 
