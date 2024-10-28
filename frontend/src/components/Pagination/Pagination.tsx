c788ddaa5adb55eff11f3c4ea0af9e51d07c38f1
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-10-28 01:32:00 -0300

    Adding PAgination startpage

--- frontend/src/components/Pagination/Pagination.tsx
+++ frontend/src/components/Pagination/Pagination.tsx
@@ -12,26 +12,39 @@ const Pagination: React.FC<PaginationProps> = ({
   limit,
   currentPage,
   handlePagination,
-}) => (
-  <div className="pagination-buttons">
-    {[...Array(Math.ceil(totalProducts / limit))].map((_, index) => (
+}) => {
+  const totalPages = Math.ceil(totalProducts / limit);
+  const paginationButtons = [];
+  const startPage = Math.floor((currentPage - 1) / 3) * 3 + 1;
+  const endPage = Math.min(startPage + 2, totalPages);
+
+  for (let i = startPage; i <= endPage; i++) {
+    paginationButtons.push(
       <button
-        key={index}
+        key={i}
         className="pagination-button"
-        onClick={() => handlePagination(index + 1)}
-        disabled={index + 1 === currentPage}
+        onClick={() => handlePagination(i)}
+        disabled={i === currentPage}
       >
-        {index + 1}
+        {i}
       </button>
-    ))}
-    <button
-      className="pagination-button pagination-button-next"
-      onClick={() => handlePagination(currentPage + 1)}
-      disabled={currentPage >= Math.ceil(totalProducts / limit)}
-    >
-      Next
-    </button>
-  </div>
-);
+    );
+  }
+
+  return (
+    <div className="pagination-buttons">
+      {paginationButtons}
+      {totalPages > endPage && (
+        <button
+          className="pagination-button pagination-button-next"
+          onClick={() => handlePagination(endPage + 1)}
+          disabled={currentPage >= totalPages}
+        >
+          Next
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Pagination;
